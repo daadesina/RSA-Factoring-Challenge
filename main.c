c6e4d71c0c7f693d662f24fbacb9520cac7d57df
daadasina <daadesina1@gmail.com>
2024-01-28 05:59:10 +0000

    done

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ int main(int argc, char* argv[])
 	myFile = fopen(argv[1], "r");
 	fgets(buffer, 300, myFile);
 
-	/*while(fgets(buffer, 300, myFile) != NULL)
-	{*/
+	while(fgets(buffer, 300, myFile) != NULL)
+	{
 		num = strtoll(buffer, NULL, 0);
 		first = logic(num);
 		second = num / first;
 		printf("%lld=%lld*%lld\n", num, second, first);
-	//}
+	}
 
 	fclose(myFile);
 
